6f838916a900068666649ca2a10eb72f34c43ac7
DeAsianDragon <joshlin.ling@yahoo.com>
2023-11-21 07:00:47 -0700

    search

---
+++ scripts/search.js
@@ -0,0 +1,162 @@
+document.addEventListener("DOMContentLoaded", () => {
+    populateLocations();
+    populateParkTypes();
+  
+    let locationSelect = document.getElementById("locationList");
+    locationSelect.addEventListener("change", filterParksByLocationAndType);
+  
+    let parkTypeSelect = document.getElementById("parkTypeList");
+    parkTypeSelect.addEventListener("change", filterParksByLocationAndType);
+  });
+  
+  const populateLocations = () => {
+    const locationList = document.getElementById("locationList");
+  
+  
+  
+    for (let location of locationsArray) {
+      const option = new Option(location);
+      locationList.appendChild(option);
+    }
+  };
+  
+  const populateParkTypes = () => {
+    const parkTypeList = document.getElementById("parkTypeList");
+    for (let type of parkTypesArray) {
+      const option = new Option(type);
+      parkTypeList.appendChild(option);
+    }
+  };
+  
+  const filterParksByLocationAndType = () => {
+    let locationSelect = document.getElementById("locationList").value;
+    let typeSelected = document.getElementById("parkTypeList").value;
+  
+    let filteredParks = nationalParksArray;
+  
+    if (locationSelect !== "Show All" && locationSelect !== "Select State") {
+      filteredParks = filteredParks.filter(
+        (park) => park.State == locationSelect
+      );
+    }
+  
+    if (typeSelected !== "Show All" && typeSelected !== "Select Type") {
+      filteredParks = filteredParks.filter((park) =>
+        park.LocationName.includes(typeSelected)
+      );
+    }
+  
+    if (filteredParks.length === 0) {
+      displayNoResultsMessage();
+    } else {
+      displayAllParks(filteredParks);
+    }
+  };
+  
+  const displayNoResultsMessage = () => {
+    const parksContainer = document.querySelector("#content");
+    parksContainer.innerHTML = "";
+  
+    const noResultsMessage = document.createElement("p");
+    noResultsMessage.classList.add("noResultsText");
+    noResultsMessage.innerText =
+    "No Results Match Your Filters";
+  
+    parksContainer.appendChild(noResultsMessage);
+  };
+  
+  const displayAllParks = (nationalParksArray) => {
+    const parksContainer = document.querySelector("#content");
+    parksContainer.innerHTML = "";
+  
+    nationalParksArray.forEach((park) => {
+      displayPark(park, parksContainer);
+    });
+  };
+  
+  const displayPark = (park, parkDiv) => {
+    const parkContainer = document.createElement("div");
+    parkContainer.classList.add("parkContainer");
+  
+    parkDiv.appendChild(parkContainer);
+  
+    addParkName(park, parkContainer);
+  
+    addParkAddress(park, parkContainer);
+    addCity(park, parkContainer);
+    addState(park, parkContainer);
+    addZipCode(park, parkContainer);
+    addPhone(park, parkContainer);
+    addFax(park, parkContainer);
+    addLatitude(park,parkContainer);
+    addLongitude(park,parkContainer);
+    if (park.Visit){
+      addLinkOfPark(park, parkContainer)
+  }
+  
+  
+  };
+  
+  const addParkName = (park, parkDiv) => {
+    const parkHeader = document.createElement("h4");
+    parkHeader.innerText = park.LocationName;
+  
+    parkDiv.appendChild(parkHeader);
+  };
+  
+  const addParkAddress = (park, parkDiv) => {
+    const parkAddress = document.createElement("p");
+    parkAddress.innerText = `Address: ${park.Address}`;
+    parkDiv.appendChild(parkAddress);
+  };
+  
+  const addCity = (park, parkDiv) => {
+    const cityOfPark = document.createElement("p");
+    cityOfPark.innerText = `City: ${park.City}`;
+    parkDiv.appendChild(cityOfPark);
+  };
+  
+  const addState = (park, parkDiv) => {
+    const stateOfPark = document.createElement("p");
+    stateOfPark.innerText = `State: ${park.State}`;
+    parkDiv.appendChild(stateOfPark);
+  };
+  
+  const addZipCode = (park, parkDiv) => {
+    const zipOfPark = document.createElement("p");
+    zipOfPark.innerText = `Zip Code: ${park.ZipCode}`;
+    parkDiv.appendChild(zipOfPark);
+  };
+  
+  const addPhone = (park, parkDiv) => {
+    const phoneOfPark = document.createElement("p");
+    phoneOfPark.innerText = `Phone: ${park.Phone}`;
+    parkDiv.appendChild(phoneOfPark);
+  };
+  
+  const addFax = (park, parkDiv) => {
+    const faxOfPark = document.createElement("p");
+    faxOfPark.innerText = `Fax: ${park.Fax}`;
+    parkDiv.appendChild(faxOfPark);
+  };
+  
+  function addLatitude(park,parkDiv){
+      const parkLatitude=document.createElement("p");
+      parkLatitude.innerText="Latitude: "+park.Latitude;
+      parkDiv.appendChild(parkLatitude);
+  }
+  
+  function addLongitude(park,parkDiv){
+      const parkLongitude=document.createElement("p");
+      parkLongitude.innerText="Longitude: "+park.Longitude;
+      parkDiv.appendChild(parkLongitude);}
+  
+  const addLinkOfPark = (park,parkDiv) => {
+    const parkLink = document.createElement("a")
+    parkLink.classList.add("linkOfPark");
+    parkLink.href = park.Visit;
+  
+    parkLink.textContent = "More info";
+    parkLink.target = "blank"
+    parkDiv.appendChild(parkLink)
+  }
